71e88fab302bbed0c3f61918766c6c01933d83cb
mariem mkadem <mariem.mkadem@esprit.tn>
2024-08-17 15:01:02 +0100

    last update and check 3

--- src/app/admin/list-credits/list-credits.component.ts
+++ src/app/admin/list-credits/list-credits.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CreditRequestService } from 'src/app/Services/credit-request.service';
-import { TransactionService } from 'src/app/Services/transaction.service';
+import { Client } from 'src/app/core/models/Client';
 import { Credit } from 'src/app/core/models/CreditRequest';
+import { DatePipe } from '@angular/common';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-list-credits',
   templateUrl: './list-credits.component.html',
   styleUrls: ['./list-credits.component.css']
 })
-export class ListCreditsComponent implements OnInit{
+export class ListCreditsComponent implements OnInit {
+
   
-credits: Credit[]=[]
-loading: boolean = false;
-error: string | null = null;
-  constructor(private creditRequestService:CreditRequestService){}
+  credits: Credit[] = [];
+  loading: boolean = false;
+  error: string | null = null;
+  selectedUser: any;
+  modalOpen = false;
+  constructor(private creditRequestService: CreditRequestService, private datePipe: DatePipe) {}
 
   ngOnInit(): void {
     this.loading = true;
+
+    // Fetch all credit requests
     this.creditRequestService.getAllCreditRequests().subscribe(
       (data: Credit[]) => {
         this.credits = data;
         this.loading = false;
       },
-      (err) => {
-        this.error = 'An error occurred while fetching credit requests.';
+      (error) => {
+        this.error = "Failed to load credits";
         this.loading = false;
       }
     );
@@ -36,11 +43,30 @@ error: string | null = null;
     });
   }
 
-  rejectCredit(creditId:number):void{
-    this.creditRequestService.rejectCreditRequest(creditId).subscribe((rejectedCredit)=>{
-      this.credits=this.credits.map(credit =>
-        credit.id ===creditId ? rejectedCredit:credit);
-  });
+  rejectCredit(creditId: number): void {
+    this.creditRequestService.rejectCreditRequest(creditId).subscribe((rejectedCredit) => {
+      this.credits = this.credits.map(credit =>
+        credit.id === creditId ? rejectedCredit : credit
+      );
+    });
   }
 
+  consulterUtilisateur(credit: Credit) {
+    this.selectedUser = credit.user;
+    this.modalOpen = true;
+
+    // Manually trigger modal opening
+    const modalElement = document.getElementById('userDetailsModal') as any;
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+  getFormattedDate(date?: [number, number, number]): string {
+    if (date) {
+      return this.datePipe.transform(new Date(date[0], date[1] - 1, date[2]), 'dd-MM-yyyy') || '--';
+    }
+    return '--';
+  }
 }
